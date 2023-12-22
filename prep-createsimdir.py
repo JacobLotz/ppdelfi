dc22322997336130adcc94cf74a362b1acb71b35
JacobLotz <jacob.lotz@gmail.com>
2023-12-22 14:29:08 +0100

    update

--- prep-createsimdir.py
+++ prep-createsimdir.py
@@ -25,23 +25,25 @@ def CreateRunFile(n_, dname_, name_, k_, mname_):
 	f.write("#SBATCH --error=error.err\n")
 	f.write("#SBATCH -n 24\n")
 	f.write("#SBATCH -N 1\n")
-	f.write("#SBATCH --time=1-00:0:0\n")
+	f.write("#SBATCH --time=2-00:0:0\n")
 	f.write("#SBATCH --mem=96G\n")
 
 	f.write("\n")
 	f.write("srun ~/delfi/build/delfi  \\\n")
-	f.write("   --physics PTNavStoPSPG \\\n")
-	f.write("   -b local.bcs           \\\n")
+	f.write("   --physics SpTiNavStoPSPG \\\n")
+	f.write("   -b spacetime.bcs           \\\n")
 	f.write("   -dt 1                  \\\n")
-	f.write("   -tf 100                \\\n")
+	f.write("   -tf 300                \\\n")
 	f.write("   -fb 5                  \\\n")
 	f.write("   -ptc 1e-5              \\\n")
 	f.write("   -o 2                   \\\n")
 	f.write("   -r 0                   \\\n")
+	f.write("   -li 2000               \\\n")
+	f.write("   -fb 5                  \\\n")
 	f.write("   -lt 0.01               \\\n")
 	f.write("   -nt 0.01               \\\n")
 	f.write("   --offline              \\\n")
-	f.write("   --hyper-reduce         \\\n")
+	f.write("   -ht 1                  \\\n")
 	f.write("   -id " + str(n_) + " \\\n")
 	f.write("   -m " + mname_ + ".mesh \\\n")
 	f.write("   -k " + str(k_) + " \\\n")
@@ -61,12 +63,12 @@ def MeshRange(mn_base_, n_):
 	return names
 
 def CreateMesh(extr_, mn_):
-	# NCP of the course mesh
-	nrad  = 15; 
-	ntip  = 10; 
-	ntail = 20; 
-	nwake = 45;
-	nextr = 7;
+	# NCP of the mesh
+	nrad  = 68; 
+	ntip  = 28; 
+	ntail = 48; 
+	nwake = 79;
+	nextr = 49;
 
 	# Other settings
 	tf   = 0.1;
@@ -92,14 +94,14 @@ def CreateMesh(extr_, mn_):
 
 
 # Input
-maindirname = "test"
+maindirname = "simulations"
 dirname = "run"
 
 filename = "run"
 meshnamebase = "naca-cmesh"
 re_min = 200
 re_max = 800
-n_runs = 11
+n_runs = 30
 
 T_min = 4
 T_max = 8
@@ -110,7 +112,7 @@ dirname = maindirname + "/" + dirname
 
 kr      = KappaRange(re_min, re_max, n_runs)
 mnr     = MeshRange(meshnamebase, n_runs)
-periodr = np.linspace(T_in, T_max, n_runs)
+periodr = np.linspace(T_min, T_max, n_runs)
 
 for i in range(0, n_runs):
 	k = kr[i]
