520d057e32396782c0d8fbd340adf0e90c9e4655
JacobLotz <jacob.lotz@gmail.com>
2024-01-08 09:39:46 +0100

    update

--- prep-createsimdir.py
+++ prep-createsimdir.py
@@ -20,28 +20,29 @@ def CreateRunFile(n_, dname_, name_, k_, mname_):
 	f.write("#\n")
 	f.write("#SBATCH --job-name=R" + str(n_).zfill(3)+"\n")
 	f.write("#SBATCH --partition=compute\n")
-	f.write("#SBATCH --account=innovation\n")
+	f.write("#SBATCH --account=research-3me-mtt\n")
 	f.write("#SBATCH --output=log\n")
 	f.write("#SBATCH --error=error.err\n")
-	f.write("#SBATCH -n 24\n")
-	f.write("#SBATCH -N 1\n")
-	f.write("#SBATCH --time=2-00:0:0\n")
-	f.write("#SBATCH --mem=96G\n")
+	f.write("#SBATCH --ntasks=32\n")
+	f.write("#SBATCH --cpus-per-task=1\n")
+	f.write("#SBATCH --nodes=1\n")
+	f.write("#SBATCH --mem-per-cpu=4G\n")
+	f.write("#SBATCH --time=5-00:0:0\n")
 
 	f.write("\n")
 	f.write("srun ~/delfi/build/delfi  \\\n")
 	f.write("   --physics SpTiNavStoPSPG \\\n")
 	f.write("   -b spacetime.bcs           \\\n")
 	f.write("   -dt 1                  \\\n")
-	f.write("   -tf 300                \\\n")
+	f.write("   -tf 3000               \\\n")
 	f.write("   -fb 5                  \\\n")
 	f.write("   -ptc 1e-5              \\\n")
 	f.write("   -o 2                   \\\n")
 	f.write("   -r 0                   \\\n")
 	f.write("   -li 2000               \\\n")
-	f.write("   -fb 5                  \\\n")
 	f.write("   -lt 0.01               \\\n")
 	f.write("   -nt 0.01               \\\n")
+	f.write("   -ni 5                  \\\n")
 	f.write("   --offline              \\\n")
 	f.write("   -ht 1                  \\\n")
 	f.write("   -id " + str(n_) + " \\\n")
@@ -64,11 +65,11 @@ def MeshRange(mn_base_, n_):
 
 def CreateMesh(extr_, mn_):
 	# NCP of the mesh
-	nrad  = 68; 
+	nbnd  = 68; 
 	ntip  = 28; 
 	ntail = 48; 
 	nwake = 79;
-	nextr = 49;
+	ntime = 49;
 
 	# Other settings
 	tf   = 0.1;
@@ -79,19 +80,19 @@ def CreateMesh(extr_, mn_):
 	aoa  = 0.0;
 
 	# Mesh stretches
-	srad = 3.0;
+	sbnd = 3.0;
 	stip = 1.2;
 	stail = 1.2;
 	sw   = 1.2;
 
 	# Create mesh
-	script = "/home/jelotz/prog/delfi/build/navierstokes/nacast/mesher"
+	script = "/home/jelotz/prog/delfi/build/navierstokes/nacast/mesher2"
 
 	subprocess.run([script, "-tf", str(tf), "-b", str(b), "-w", str(w)
-		, "-srad", str(srad), "-stip", str(stip), "-sw" , str(sw), "-stail" , str(stail)
-		,"-nrad", str(nrad),  "-ntip", str(ntip), "-ntail"
+		, "-sbnd", str(sbnd), "-stip", str(stip), "-sw" , str(sw), "-stail" , str(stail)
+		,"-nbnd", str(nbnd),  "-ntip", str(ntip), "-ntail"
 		, str(ntail), "-nwake", str(nwake),"-m", mn_, "-aoa", str(aoa),
-		"-nextr", str(nextr),"-ha", str(ha)])
+		"-ntime", str(ntime),"-ha", str(ha)])
 
 
 
